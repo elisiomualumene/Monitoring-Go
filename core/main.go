6ea71a2899562e4737ed3468593b0c04a95b6601
elisiomualumene <elisiomualumene@gmail.com>
2023-11-19 21:28:49 +0100

    :fire: new metric

--- core/main.go
+++ core/main.go
@@ -17,14 +17,27 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_http_requests_total",
+	Help: "Count of all http request for goapp",
+}, []string{})
+
 func main() {
 	r := prometheus.NewRegistry()
 
 	r.MustRegister(onlineUsers)
+	r.MustRegister(httpRequestTotal)
 
-	go func(){
+	go func() {
 		onlineUsers.Set(float64(rand.Intn(2000)))
 	}()
+
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello Endpoint"))
+	})
+
+	http.Handle("/", promhttp.InstrumentHandlerCounter(httpRequestTotal, home))
 
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
